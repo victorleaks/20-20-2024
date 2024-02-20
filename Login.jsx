70740edd328beb9d106560595f9e1c1b209f34f4
Victor <112430306+victorleaks@users.noreply.github.com>
2024-02-20 10:16:27 -0300

    Add files via upload

---
+++ Login.jsx
@@ -0,0 +1,18 @@
+import{
+    txtLogin,
+    txtSenha
+} from '../App';
+
+var login = "teste";
+var senha = "teste";
+
+//var txtLogin = document.getElementById("txtLogin");
+//var txtSenha = document.getElementById("txtSenha");
+
+export const fazerLogin = (e) => {
+    if (txtLogin.value == login && txtSenha.value == senha) {
+        console.log("Acesso liberado");
+    } else {
+        console.log("Login ou senha inválidos");
+    }
+}
